df9ce9b746ba65b170c9fc245dd6e58bf8a3bd2b
koacherkartik <176943983+koacherkartik@users.noreply.github.com>
2026-02-19 22:51:56 +0530

    Create user.java

---
+++ user.java
@@ -0,0 +1,21 @@
+package model;
+
+public class User {
+    private String username;
+    private String password;
+    private Wallet wallet;
+
+    public User(String username, String password) {
+        this.username = username;
+        this.password = password;
+        this.wallet = new Wallet();
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public Wallet getWallet() {
+        return wallet;
+    }
+}
